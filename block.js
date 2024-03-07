9ab63ebd87e42059cc743ed64340bd252339873c
nima makhmali <154757931+nimamakhmali@users.noreply.github.com>
2024-03-07 14:09:13 +0330

    start

---
+++ block.js
@@ -0,0 +1,11 @@
+// Nima makhmali  blockChain project
+class Block
+{
+  constractor(timestamp, lasthash, hash, data)
+  {
+    this.timestamp = timestamp;
+    this.lasthash = lasthash;
+    this.hash = hash;
+    this.data = data;
+  }
+}
