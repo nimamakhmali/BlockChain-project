37b2a71b94ca1081bfa8d58ad39f43a7b19fdbf3
nimamakhmali <nmkhmly790@gmail.com>
2024-12-11 17:26:54 +0100

    BlockChain class

--- block.js
+++ block.js
@@ -1,11 +1,36 @@
 // Nima makhmali  blockChain project
-class Block
-{
-  constractor(timestamp, lasthash, hash, data)
-  {
-    this.timestamp = timestamp;
-    this.lasthash = lasthash;
-    this.hash = hash;
+class Block{
+  constructor(data,hash,lasthash){
+    //this.timestamp = timestamp;
     this.data = data;
+    this.hash = hash;
+    this.lasthash = lasthash;
   }
 }
+
+class BlockChain{
+  constructor(){
+    const genesis = new Block('gendata','genhash','genlasthash');
+    this.chain = [genesis];
+  }
+
+  addBlock(data){
+    const lasthash = this.chain[this.chain.length-1].hash;
+    const hash = hashFunc(data + lasthash);
+    const block = new Block(data,hash,lasthash);
+    this.chain.push(block);
+  }
+}
+
+
+
+const hashFunc = (input)=>{
+  return "*" + input + "*";
+}
+
+
+const fooBlock = new BlockChain();
+fooBlock.addBlock('one');
+fooBlock.addBlock('two');
+fooBlock.addBlock('three');
+console.log(fooBlock);
